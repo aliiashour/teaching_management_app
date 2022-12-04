07df792afdd9fb4c05d19ae6a8ce24a22f9d80f1
ali_ashour <aliashour592@gmail.com>
2022-12-04 12:04:48 +0200

    adding basic styling files and structures

---
+++ layout/js/main_js.js
@@ -0,0 +1,26 @@
+
+// set menu list fixed height dynamically
+var wheight = $(window).outerHeight() ,
+header_height = $(".navbar").innerHeight() ; 
+$(".menu").height( wheight - header_height )  ; 
+$(".data").height( wheight - header_height )  ; 
+
+
+
+function re_size_page(){
+    // Justify Slider Height
+    var wheight = $(window).outerHeight() ,
+        header_height = $(".navbar").innerHeight() ; 
+    $(".menu").height( wheight - header_height ) ; 
+    $(".data").height( wheight - header_height )  ; 
+    console.log(header_height) ; 
+}
+
+// show/hide category items
+$(".category-dropdown-icon").each(function(){
+    $(this).on("click", function(){
+        let target = $(this).data("target") ; 
+        $("#"+target).slideToggle("fast") ; 
+    });
+});
+
